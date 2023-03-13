a13795a29f92ed05a1a65c753679e04731bf452d
eman obeidat <amoonobeidat43@gmail.com>
2023-03-13 19:01:23 +0000

    not completed

--- accounting.js
+++ accounting.js
@@ -1,2 +1,35 @@
-'use strict'
-console.log(localStorage);
+`use strict`
+let acc=localStorage.getItem('info');
+let tableEL=document.getElementById("Tb");
+function EmployeeInfo(Department,employeeNum,averageSalary,totalSalary)
+{
+    this.Department=Department;
+    this.employeeNum=numOfEmployee();
+    this.averageSalary=0;
+}
+EmployeeInfo.prototype.renderTable= function(){
+
+    let trEL = document.createElement('tr');
+   tableEL.appendChild(trEL);
+
+  
+    let tdEl = document.createElement('td');
+    tdEl.textContent=this.department;
+    trEL.appendChild(tdEl);
+  
+    let tdEl2 = document.createElement('td');
+    tdEl2.textContent=numOfEmployee();
+    trEL.appendChild(tdEl2);
+  
+  
+    let tdEl3 = document.createElement('td');
+    tdEl3.textContent="hello";
+    trEL.appendChild(tdEl3);
+
+    let tdEl4= document.createElement('td');
+    tdEl4.textContent=numOfEmployee();
+    trEL.appendChild(tdEl4);
+      
+  }
+  renderTable();
+ 
--- app.js
+++ app.js
@@ -133,7 +133,15 @@ function getData() {
   let ObjArr = JSON.parse(getInfo); //take the array that i git from LS and convert it to array of obj
   console.log(ObjArr);
   //reinstantiation of new instances
-  let newOb= new Employee(ObjArr[ObjArr.length-1].employeeId,ObjArr[ObjArr.length-1].fullName,ObjArr[ObjArr.length-1].department,ObjArr[ObjArr.length-1].level,ObjArr[ObjArr.length-1].imageUrl,);
-  newOb.render();//3.call the render method
+  if (objArray != null) {
+    for (let i = 0; i < ObjArr.length; i++)//render all information iside the array
+    {
+      new Employee(ObjArr[i].employeeId, ObjArr[i].fullName, ObjArr[i].department, ObjArr[i].level, ObjArr[i].imageUrl); //new object inside constructor to be able for render
+    }
+  }
+  renderAll();//3.call the render method
+  
+  // let newOb= new Employee(ObjArr[ObjArr.length-1].employeeId,ObjArr[ObjArr.length-1].fullName,ObjArr[ObjArr.length-1].department,ObjArr[ObjArr.length-1].level,ObjArr[ObjArr.length-1].imageUrl,);
+  // newOb.render();
 }
 getData();
